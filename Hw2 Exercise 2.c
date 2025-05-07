69946f9e1dc3d27b08d5de383cce4f13a2044a74
Muhammed Enes Değer <148690671+EnesDeger@users.noreply.github.com>
2025-05-07 18:48:52 +0300

    Create Hw2 Exercise 2.c

---
+++ Hw2 Exercise 2.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void Hunerler ( float itemPrice, float* donationTotal , float* profitTotal);
+
+int main( void ){
+    
+    printf("This program gets the profits and donations:\n");
+    
+    float itemPrice, total_donation = 0, total_profit = 0;
+    
+    for( int i=0; i<50; i++ ){
+        itemPrice = rand() %21000 ;
+        itemPrice = itemPrice /1000;
+        Hunerler(itemPrice, &total_donation, &total_profit);
+        printf("Item is %.2f \n", itemPrice);
+        }
+        
+    printf("Donation is %.2f\n", total_donation);
+    printf("Profit is %.2f", total_profit);
+}
+void Hunerler ( float itemPrice, float *donationTotal , float *profitTotal){
+    
+    int profit   = (int)itemPrice;
+    float donation = itemPrice - profit;
+    
+    *donationTotal = *donationTotal + donation;
+    *profitTotal   = *profitTotal   + profit;
+}
